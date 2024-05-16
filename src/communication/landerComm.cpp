c1bf4351849a1f577a3be24481f138922137dd6c
Henri Vanhuynegem <H.Vanhuynegem@student.tudelft.nl>
2024-05-16 10:34:18 +0200

    code now compiles

--- src/communication/landerComm.cpp
+++ src/communication/landerComm.cpp
@@ -2,11 +2,15 @@
 #include <msp430.h>
 #include <stdint.h>
 
+#define TX_BUFFER_SIZE 256   // Reduced buffer size
+#define RX_BUFFER_SIZE 256   // Reduced buffer size
 
-#define TX_BUFFER_SIZE 1024
 volatile uint8_t tx_buffer[TX_BUFFER_SIZE];
 volatile uint16_t tx_in_index = 0;  // Index where data is added to the buffer
 volatile uint16_t tx_out_index = 0; // Index where data is sent from the buffer
+
+volatile uint8_t rx_buffer[RX_BUFFER_SIZE];
+volatile uint16_t rx_index = 0;     // Index for receiving data
 
 // Function to add data to the transmit buffer
 void tx_buffer_add_byte(uint8_t byte) {
@@ -14,26 +18,22 @@ void tx_buffer_add_byte(uint8_t byte) {
     if (tx_in_index >= TX_BUFFER_SIZE) tx_in_index = 0; // Circular buffer
 }
 
-
-void initialize_UART(int baudrate);
-void slip_encode(uint8_t *buffer, uint16_t length);
-void slip_decode(uint8_t *buffer, uint16_t *received_length);
-
-void initialize_UART(int baudrate){
-    // Assuming SMCLK has been configured to 16MHz, calculate BRW for 9600 baud
+// UART initialization function
+void initialize_UART(int baudrate) {
     UCA0CTL1 |= UCSWRST;        // Put eUSCI in reset
-    UCA0CTL1 |= UCSSEL__SMCLK;  // SMCLK
+    UCA0CTL1 |= UCSSEL__SMCLK;  // Select SMCLK
 
-    // Configure baud rate
-    UCA0BR0 = 1667;     // 9600 baud if SMCLK = 16MHz (16MHz/9600 = 1667)
-    UCA0BR1 = 0;        // Baud Rate Control
-    UCA0MCTLW = 0x2000 | UCOS16 | UCBRF_1; // Modulation UCBRSx=0, UCBRFx=1, oversampling
-    //UCA0MCTLW = 0x5551; // Modulation UCBRSx=0, UCBRFx=1, oversampling
+    // Configure baud rate for 9600 with 1MHz clock
+    UCA0BR0 = 6;                // Baud rate setting
+    UCA0BR1 = 0;                // Baud Rate Control
+    UCA0MCTLW = 0x2081;         // Modulation UCBRSx=0x20, UCBRFx=8, oversampling
 
-    UCA0CTL1 &= ~UCSWRST;   // Initialize eUSCI
-    UCA0IE |= UCRXIE;       // Enable USCI_A0 RX interrupt
-};
+    UCA0CTL1 &= ~UCSWRST;       // Initialize eUSCI
+    UCA0IE |= UCRXIE;           // Enable USCI_A0 RX interrupt
+    UCA0IE |= UCTXIE;           // Enable USCI_A0 TX interrupt
+}
 
+// SLIP encoding function
 void slip_encode(uint8_t *buffer, uint16_t length) {
     const uint8_t END = 0xC0;
     const uint8_t ESC = 0xDB;
@@ -61,7 +61,7 @@ void slip_encode(uint8_t *buffer, uint16_t length) {
     UCA0IE |= UCTXIE;         // Enable transmit interrupt
 }
 
-
+// SLIP decoding function
 void slip_decode(uint8_t *buffer, uint16_t *received_length) {
     const uint8_t END = 0xC0;
     const uint8_t ESC = 0xDB;
@@ -100,25 +100,25 @@ void slip_decode(uint8_t *buffer, uint16_t *received_length) {
 }
 
 
-// ISR for receiving UART data
+// ISR for receiving and transmitting UART data
 #pragma vector=USCI_A0_VECTOR
 __interrupt void USCI_A0_ISR(void) {
     switch(__even_in_range(UCA0IV, USCI_UART_UCTXCPTIFG)) {
         case USCI_NONE: break;
         case USCI_UART_UCRXIFG: // receive interrupt
             rx_buffer[rx_index++] = UCA0RXBUF;  // Read RX buffer and increment index
-            if (rx_index >= BUFFER_SIZE) {
+            if (rx_index >= RX_BUFFER_SIZE) {
                 rx_index = 0;  // Reset index if buffer is full
             }
             break;
         case USCI_UART_UCTXIFG: // transmit interrupt
-                    if (tx_out_index != tx_in_index) {
-                        UCA0TXBUF = tx_buffer[tx_out_index++];
-                        if (tx_out_index >= TX_BUFFER_SIZE) tx_out_index = 0;  // Circular buffer
-                    } else {
-                        UCA0IE &= ~UCTXIE;  // Disable TX interrupt if no more data to send
-                    }
-                    break;
-                default: break;
+            if (tx_out_index != tx_in_index) {
+                UCA0TXBUF = tx_buffer[tx_out_index++];
+                if (tx_out_index >= TX_BUFFER_SIZE) tx_out_index = 0;  // Circular buffer
+            } else {
+                UCA0IE &= ~UCTXIE;  // Disable TX interrupt if no more data to send
+            }
+            break;
+        default: break;
     }
 }
