6489286019832e565a8a44c29569c03b3e63e250
Henri Vanhuynegem <H.Vanhuynegem@student.tudelft.nl>
2024-05-21 14:40:02 +0200

    update to landerComm

--- include/communication/landerComm.h
+++ include/communication/landerComm.h
@@ -7,10 +7,8 @@
 /*
  * This function initializes the UART ports using a baud rate.
  *
- * parameters:
- *  int baudrate - the rate at which data is sent in baud/s
  */
-void initialize_UART(int baudrate);
+void initialize_UART(void);
 
 /*
  * This function encodes an array and sends it through the two UART pins.
@@ -19,7 +17,7 @@ void initialize_UART(int baudrate);
  *   uint8_t *buffer: message to be sent
  *   uint16_t length: length of packet to be sent
  */
-void slip_encode(uint8_t *buffer, uint16_t length);
+void slip_encode(const uint8_t *buffer, uint16_t length);
 
 /*
  * This function decodes an array and receives it through the two UART pins.
@@ -29,5 +27,19 @@ void slip_encode(uint8_t *buffer, uint16_t length);
  *   uint16_t *received_length: received length of received packet
  */
 void slip_decode(uint8_t *buffer, uint16_t *received_length);
+
+/*
+ * This function initialises the ACLK at a frequency of 32.768 Hz
+ *
+ */
+void initialize_clock(void);
+
+/*
+ * This function initialises a timer for a certain amount of clk cycles
+ *
+ * parameters:
+ *  int cycles: the amount of cycles that need to be run.
+ */
+void start_timer(unsigned int  cycles);
 
 #endif // LANDER_COMM_H
