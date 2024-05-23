f71ade46dd8e2bdae3e2f895b8f2a40d2464e696
Diederik02 <diederik6.aris@gmail.com>
2024-05-23 15:32:59 +0200

    Update actuatorControl.cpp

--- src/deployment/actuatorControl.cpp
+++ src/deployment/actuatorControl.cpp
@@ -14,15 +14,22 @@ bool read_umbilicalcord_pin_rover(void) {
     return (P3IN & BIT0) != 0;              // Return the status of P3.0
 }
 
-// Function to initialize all 3 NEA's
-void initialize_all_neas(void) {
+// Function to initialize Super cap voltage pin
+void initialize_super_cap_voltage_pin(void) {
 
-    // Configure GPIO
     PM5CTL0 &= ~LOCKLPM5;                   // Disable the GPIO power-on default high-impedance mode
-    P1DIR &= ~BIT0;                         // Set P1.0 as input pin (ARM power for all NEAS)
+    P3DIR &= ~BIT3;                         // Set P3.3 as input pin (Supercap voltage for all NEAS)
+    P3SEL1 |= BIT3;                         // Configure P3.3 for ADC
+    P3SEL0 |= BIT3;
+}
+
+// Function to initialize all 3 NEA's
+void initialize_all_nea_pins(void) {
+
+    PM5CTL0 &= ~LOCKLPM5;                   // Disable the GPIO power-on default high-impedance mode
 
     // NEA 1
-    P3DIR &= ~BIT3;                         // Set P3.3 as input pin (NEA Ready for NEA 1)
+    P1DIR &= ~BIT2;                         // Set P1.2 as input pin (NEA Ready for NEA 1)
     P2DIR |= BIT4;                          // Set P2.4 as output pin (NEA Flag for NEA 1)
     P2OUT &= ~BIT4;                         // Initialize P2.4 to low (don't activate NEA 1)
 
@@ -37,10 +44,9 @@ void initialize_all_neas(void) {
     P2OUT &= ~BIT7;                         // Initialize P2.7 to low (don't activate NEA 3)
 }
 
-// Function to read the status of P3.0
-bool read_ARM_power_status(void) {
-    return (P1IN & BIT0) != 0;              // Return the status of P1.0 (ARM power for all NEAS)
-}
+
+
+
 
 // Function to read the status of the NEA ready bit
 bool read_NEAready_status(volatile uint8_t *port_in, uint8_t pin) {
@@ -53,7 +59,13 @@ void RDS_electronics_status_check(void){
     initialize_umbilicalcord_pin_rover();
     initialize_all_neas();
 
-    //Send a different massage to the rover depending on the status of the umbilical cord to the rover
-    if(read_umbilicalcord_pin_rover())
+    //Send a different massage to the lander depending on the status of the umbilical cord to the rover
+    if(read_umbilicalcord_pin_rover()){
+        send_message(uint8_t msg_type, const uint8_t *payload, uint8_t length) //TBD massage and data format
+    }
+    else{
+        send_message(uint8_t msg_type, const uint8_t *payload, uint8_t length) //TBD massage and data format
+    }
+
 
 }
