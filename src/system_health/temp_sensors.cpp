f762a2a716985633c6037673c357e1b1ce05d123
Henri vanhuynegem <93914927+Hvanhuynegem@users.noreply.github.com>
2024-06-16 14:52:59 +0200

    frequency to temp update

--- src/system_health/temp_sensors.cpp
+++ src/system_health/temp_sensors.cpp
@@ -89,13 +89,14 @@ void initializeDCO(void) {
 
 // Function to convert frequency to temperature
 float frequency_to_temperature(float frequency) {
-    if (frequency == 0.0) {
-            return -99.0; // returns an error value that is impossible to record
-        }
-    float capacitance = 0.0000022; // Capacitance in Farads
-    float x = 1.0 / (0.4055 * 2.0 * capacitance * frequency); // Intermediate calculation using the frequency
-    float temperature = (x - 1000.0) / 3.85; // Convert intermediate value to temperature
-    return temperature; // Return the calculated temperature
+    if (frequency >= 150 && frequency <= 800) {
+        float capacitance = 0.0000022; // Capacitance in Farads
+        float x = 1.0 / (0.4055 * 2.0 * capacitance * frequency); // Intermediate calculation using the frequency
+        float temperature = (x - 1000.0) / 3.85; // Convert intermediate value to temperature
+        return temperature; // Return the calculated temperature
+    } else {
+        return -99.0;  // returns an error value that is impossible to record
+    }
 }
 
 // Function to set up Timer_B0 related to the temperature pins
